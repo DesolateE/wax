3d3fa3ed63006a20443bad76befb1b25d73ca91c
kanin sakwattanavakin <bu.nakip@gmail.com>
2021-05-06 12:52:00 +0700

    final prod

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <b-button @click="signout" type="submit" class="btn" variant="danger">Signout</b-button>
+    <b-button class="button btn-primary" @click="watch">Watch</b-button>
   <!-- <button @click="removeRow(row)">Remove</button>
   <button class="button btn-primary" @click="addRow">Add row</button> -->
     <b-table striped hover :items="items" :fields="fields">
@@ -8,18 +9,12 @@
         {{ row.index + 1 }}
       </template>
       <template v-slot:cell(accname)="row">
-        <b-form-input v-model="row.item.accname"/>
-      </template>
-      <template v-slot:cell(balance)="row">
-        {{ row.item.balance }}
-      </template>
-      <template v-slot:cell(cpu)="row">
-        {{ row.item.cpu }}
+        <b-form-input v-model="row.item.accname" @change="save"/>
       </template>
     </b-table>
     <b-button class="button btn-primary" @click="addRow">Add row</b-button>
-    <b-button @click="saveandpost" class="btn" variant="primary">SaveAndGetCPU</b-button>
-    <b-button class="button btn-primary" @click="aa">AAAAA</b-button>
+    <b-button class="button btn-danger" @click="removeRow">Remove lastRow</b-button>
+    <b-button class="button btn-primary" @click="watch">Watch</b-button>
   </div>
 </template>
 <script>
@@ -27,10 +22,10 @@ import firebase from "firebase";
 import { mapGetters } from "vuex";
 // import axios from 'axios';
 export default {
-
+  name: "Home",
   data() {
     return {
-      fields: { Index: "index",accname: "accname", balance: "balance",Cpu: "cpu" },
+      fields: { Index: "index",accname: "accname"},
       items: [
       ]
     };
@@ -45,7 +40,13 @@ export default {
   created() {
     this.userid = firebase.auth().currentUser.uid;
     this.dbRef = firebase.database().ref('users/'+this.userid);
-    
+    this.dbRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      this.items = [];
+      for (let i = 0; i < data.length; i++) {
+        this.items.push({index:i+1,accname: data[i]})
+      } 
+      });
   },
   computed: {
     ...mapGetters({
@@ -53,51 +54,22 @@ export default {
     })
   },
   methods: {
-     aa () {
-      this.dbRef('value', (snapshot) => {
-      const data = snapshot.val();
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i])
-        this.items.push({index:i+1,accname: data[i], balance: 0,cpu: ""})
-      } 
-      });
+    watch(){
+      this.$router.replace("/watch")
     },
     addRow(){
-      this.items.push({ index:this.items.length+1,accname: "", balance: 0 ,cpu: ""});
-      console.log(this.items)
+      this.items.push({ index:this.items.length+1,accname: ""});
     },
-    removeRow: function(item){
-      console.log(item);
-      this.items.pop(item);
+    removeRow(){
+      this.items.pop(this.items[this.items.length-1]);
+      this.save();
     },
-    saveandpost() {
+    save() {
       let arr = [];
       for (let i = 0; i < this.items.length; i++) {
-        arr.push(this.items[i].accname);
+          arr.push(this.items[i].accname);
       }
-      console.log(this.items)
       this.dbRef.set(arr);
-
-      // for (let i = 0; i < arr.length; i++) {
-      //   const res = await axios.post("https://chain.wax.io/v1/chain/get_account", 
-      //   JSON.stringify({"account_name": arr[i]}));
-      //   console.log(i);
-      //   console.log(res);
-      //   arr.push(this.items[i].accname);
-      // } 
-      // const res = await axios.post("https://chain.wax.io/v1/chain/get_account", 
-      // JSON.stringify({"account_name": "b5kc.wam"}));
-      // console.log(res.data)
-      // .account_name
-      //.cpu_limit.used
-      //.cpu_limit.available
-      //.cpu_limit.max
-      // .core_liquid_balance
-      // .self_delegated_bandwidth.cpu_weight
-      // this.dbRef.set({
-      //   a: "aaa"
-      // })
-      // console.log("check");
     }
     ,signout() {
        firebase
---
+++ src/components/Watch.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="home">
+    <b-button @click="signout" type="submit" class="btn" variant="danger">Signout</b-button>
+    <b-button class="button btn-primary" @click="home">Home</b-button>
+    <div>update every 60sec %cpu blue:0-70% yellow:70-100% red>100%</div>
+    <b-table striped hover :items="items" :fields="fields">
+      <template v-slot:cell(index)="row">
+        {{ row.index + 1 }}
+      </template>
+      <template v-slot:cell(accname)="row">
+        {{ row.item.accname }}
+      </template>
+      <template v-slot:cell(balance)="row">
+        {{ row.item.balance }}
+      </template>
+      <template v-slot:cell(tlm)="row">
+        {{ row.item.tlm }}
+      </template>
+      <template v-slot:cell(cpu)="row">
+        {{ row.item.cpu }}
+      </template>
+      <template v-slot:cell(cpuusage)="row" >
+          <b-progress class="mt-2" animated show-value v-if="row.item.cpuusage >= 100">
+            <b-progress-bar :value="row.item.cpuusage" variant="danger"></b-progress-bar>
+         </b-progress>
+         <b-progress class="mt-2" animated show-value v-else-if="row.item.cpuusage >= 70">
+            <b-progress-bar :value="row.item.cpuusage" variant="warning"></b-progress-bar>
+         </b-progress>
+         <b-progress class="mt-2" animated show-value v-else>
+            <b-progress-bar :value="row.item.cpuusage" variant="info"></b-progress-bar>
+         </b-progress>
+      </template>
+    </b-table>
+    <b-button class="btn" variant="success" @click="forceup">force update</b-button>
+    <b-button class="button btn-primary" @click="home">Home</b-button>
+  </div>
+</template>
+<script>
+import firebase from "firebase";
+import { mapGetters } from "vuex";
+import axios from 'axios';
+export default {
+name: "Watch",
+  data() {
+    return {
+      fields: { Index: "index",accname: "accname", balance: "balance",tlm: "tlm",cpu: "cpu",cpuusage: "cpuusage" },
+      items: [
+      ]
+    };
+  },
+  beforeCreate() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (!user) {
+          this.$router.replace("/")
+        }
+    });
+  },
+  created() {
+    this.userid = firebase.auth().currentUser.uid;
+    this.dbRef = firebase.database().ref('users/'+this.userid);
+    this.dbRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      this.items = [];
+      for (let i = 0; i < data.length; i++) {
+        this.items.push({index:i+1,accname: data[i],balance:"",tlm:"",cpu:"",cpuusage:0})
+      } 
+    });
+    this.postapi();
+    this.gettlm();
+    this.papi = setInterval(() => this.postapi(), 60000);
+    this.gtlm = setInterval(() => this.gettlm(), 60000);
+  },
+  beforeDestroy() {
+    clearInterval(this.papi)
+    clearInterval(this.gtlm)
+  },
+  computed: {
+    ...mapGetters({
+      user: "user"
+    })
+  },
+  methods: {
+    home(){
+      this.$router.replace("/home")
+    },
+    forceup(){
+        this.postapi();
+        this.gettlm();
+    },
+    async postapi() {
+      for (let i = 0; i < this.items.length; i++) {
+        const res = await axios.post("https://chain.wax.io/v1/chain/get_account", 
+        JSON.stringify({"account_name": this.items[i].accname}));
+        this.items[i].balance = res.data.core_liquid_balance;
+        // let cpuuse = (res.data.cpu_limit.used / res.data.cpu_limit.max)*100;
+        this.items[i].cpuusage = (res.data.cpu_limit.used / res.data.cpu_limit.max)*100;
+        this.items[i].cpu = "" + res.data.cpu_limit.used/1000 + "ms / " 
+                                + res.data.cpu_limit.max/1000 + "ms " 
+                                // + cpuuse + " %";
+        } 
+    },
+    async gettlm() {
+      for (let i = 0; i < this.items.length; i++) {
+        const res = await axios.post("https://chain.wax.io/v1/chain/get_currency_balance", 
+        JSON.stringify({
+          "code": "alien.worlds",
+          "account": this.items[i].accname,
+          "symbol": "TLM"
+        }));
+        this.items[i].tlm = res.data[0];
+        } 
+    },
+    signout() {
+       firebase
+       .auth()
+       .signOut()
+       .then(() => {
+         this.$router.replace("/");
+       });
+    }
+  }
+};
+</script>
